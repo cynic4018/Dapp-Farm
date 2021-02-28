eb9fbe8ae462a1c3d8a9016b5036cfab3bc781df
Cynic4018 <q.katapol@gmail.com>
2021-03-01 03:08:03 +0700

    Updates

---
+++ precache-manifest.c7b41ff1b3e321a30b810639ab1ab6c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81edb79eb48ca9992639",
+    "url": "/Dapp-Farm/static/js/runtime~main.81edb79e.js"
+  },
+  {
+    "revision": "c0ddecd49305b6af7536",
+    "url": "/Dapp-Farm/static/js/main.c0ddecd4.chunk.js"
+  },
+  {
+    "revision": "ee4053a6132f0cacbbb5",
+    "url": "/Dapp-Farm/static/js/1.ee4053a6.chunk.js"
+  },
+  {
+    "revision": "c0ddecd49305b6af7536",
+    "url": "/Dapp-Farm/static/css/main.62fb46b9.chunk.css"
+  },
+  {
+    "revision": "ee4053a6132f0cacbbb5",
+    "url": "/Dapp-Farm/static/css/1.d62fd1f7.chunk.css"
+  },
+  {
+    "revision": "2da4dcf764ceba56229f8560a44fd301",
+    "url": "/Dapp-Farm/index.html"
+  }
+];
